6817eba5baefd57cdd32eda0ac488b97bf1c7d02
Ricardo Lira de la Vega <ricardo.lira@globant.com>
2020-12-31 17:41:16 -0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ricardo Lira de la Vega on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
